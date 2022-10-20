eb4270093d1afb5b55313c44a249401a322ccab1
Niki <colt92@inbox.ru>
2022-10-21 00:39:36 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1_2();
         task3();
         task4_5();
+        task6();
+        task7();
 
     }
     public static void task1_2(){
@@ -30,5 +32,32 @@ public class Main {
         System.out.println("Фамилия сотрудника - " + lastName);
         System.out.println("Отчество сотрудника - " + middleName);
     }
+    public static void task6(){
+          String one = "135";
+          String two = "246";
+          String oneTwo = one + two;
+          StringBuilder sb = new StringBuilder();
+          for (int i = 0; i < oneTwo.length(); i++) {
+              if (i % 2 == 0){
+                  sb.append(one.charAt(i / 2));
+              }else{
+                  sb.append(two.charAt(i / 2));
+              }
+          }
+        String endingString = sb.toString();
+        System.out.println("Данные строки - " + endingString);
+    }
 
+    public static void task7() {
+        String random = "aabccddefgghiijjkk";
+        int count = 1;
+        char[] randomString = random.toCharArray();
+        for (int i = 0; i < randomString.length; i++) {
+            for (int j = i + 1; j < randomString.length; j++) {
+                if (randomString[i] == randomString[j]){
+                    System.out.print(randomString[j]);
+                }
+            }
+        }
+    }
 }
